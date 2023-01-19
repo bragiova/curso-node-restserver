b787abf897b495f93fffff0897a7cfef70e0e79e
Brayan Rivas <bragiova.rivas@gmail.com>
2023-01-18 19:19:14 -0600

    Backend básico

---
+++ models/server.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas
+        this.routes();
+    }
+
+    middlewares() {
+        // Directorio público
+        this.app.use(express.static('public'));
+
+        // CORS
+        this.app.use(cors());
+
+        // Lectura y Parseo del body
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/user'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        });
+    }
+}
+
+module.exports = Server;
